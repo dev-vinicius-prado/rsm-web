95438db1afa2b865a47590c6203523a3c0f86eb1
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-01-12 20:53:05 -0300

    feat: start template from RSM - Resource Management
    - add menu item with children item.

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,11 +3,19 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
+        id   : 'cadastros',
+        title: 'Cadastros',
+        type : 'collapsable',
+        icon : 'heroicons_outline:cog-8-tooth',
+        children: [
+            {
+                id   : 'cadastros.empresa',
+                title: 'Empresa',
+                type : 'basic',
+                link : '/cadastros/empresa',
+                icon : 'heroicons_outline:building-office-2'
+            },
+        ],
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
