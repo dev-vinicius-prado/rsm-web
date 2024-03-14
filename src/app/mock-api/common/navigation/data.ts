bc0d0da29a8785f3c030717a60cc06894f44a5bb
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-03-14 17:27:49 -0300

    feat: Optimizing project
    - removes navigations useless.

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -18,30 +18,3 @@ export const defaultNavigation: FuseNavigationItem[] = [
         ],
     }
 ];
-export const compactNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
-];
-export const futuristicNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
-];
-export const horizontalNavigation: FuseNavigationItem[] = [
-    {
-        id   : 'example',
-        title: 'Example',
-        type : 'basic',
-        icon : 'heroicons_outline:chart-pie',
-        link : '/example'
-    }
-];
