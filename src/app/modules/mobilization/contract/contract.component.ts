208f4ecc0f93aac81d5ca2c9ed31fda2036b84f8
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-03-14 21:00:45 -0300

    feat: Contract component
    - removes useless files and updates the contract components imports.

--- src/app/modules/mobilization/contract/contract.component.ts
+++ src/app/modules/mobilization/contract/contract.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-contract',
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './contract.component.html',
-  styleUrl: './contract.component.scss'
+  imports: [RouterOutlet],
+  template: '<router-outlet></router-outlet>',
 })
 export class ContractComponent {
 
