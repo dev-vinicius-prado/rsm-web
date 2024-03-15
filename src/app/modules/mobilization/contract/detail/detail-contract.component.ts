f594e18f70bb66110f5285519762b24fd62fd154
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-03-14 21:04:50 -0300

    feat: Detail contract component
    - implements the detail contract component.

---
+++ src/app/modules/mobilization/contract/detail/detail-contract.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-detail-contract',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './detail-contract.component.html',
+  styleUrl: './detail-contract.component.scss'
+})
+export class DetailContractComponent {
+
+}
