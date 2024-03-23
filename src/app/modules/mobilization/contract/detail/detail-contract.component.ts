59bf296a88a8cabee604961b5882c8559dd9c219
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-03-23 14:35:42 -0300

    feat: Detail contract component
    - implements the detail contract component.

--- src/app/modules/mobilization/contract/detail/detail-contract.component.ts
+++ src/app/modules/mobilization/contract/detail/detail-contract.component.ts
@@ -1,13 +1,80 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { TranslocoModule } from "@ngneat/transloco";
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import {
+  FormsModule,
+  ReactiveFormsModule,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from "@angular/forms";
+import { MatButtonModule } from "@angular/material/button";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatOptionModule } from "@angular/material/core";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatStepperModule } from "@angular/material/stepper";
 
 @Component({
-  selector: 'app-detail-contract',
+  selector: "app-detail-contract",
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './detail-contract.component.html',
-  styleUrl: './detail-contract.component.scss'
+  encapsulation: ViewEncapsulation.None,
+  imports: [
+    TranslocoModule,
+    MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatRadioModule,
+  ],
+  templateUrl: "./detail-contract.component.html",
+  styleUrl: "./detail-contract.component.scss",
 })
-export class DetailContractComponent {
+export class DetailContractComponent implements OnInit {
+  horizontalStepperForm: UntypedFormGroup;
 
+  /**
+   * Constructor
+   */
+  constructor(private _formBuilder: UntypedFormBuilder) {}
+
+  // -----------------------------------------------------------------------------------------------------
+  // @ Lifecycle hooks
+  // -----------------------------------------------------------------------------------------------------
+
+  /**
+   * On init
+   */
+  ngOnInit(): void {
+    // Horizontal stepper form
+    this.horizontalStepperForm = this._formBuilder.group({
+      step1: this._formBuilder.group({
+        email: ["", [Validators.required, Validators.email]],
+        country: ["", Validators.required],
+        language: ["", Validators.required],
+      }),
+      step2: this._formBuilder.group({
+        firstName: ["", Validators.required],
+        lastName: ["", Validators.required],
+        userName: ["", Validators.required],
+        about: [""],
+      }),
+      step3: this._formBuilder.group({
+        byEmail: this._formBuilder.group({
+          companyNews: [true],
+          featuredProducts: [false],
+          messages: [true],
+        }),
+        pushNotifications: ["everything", Validators.required],
+      }),
+    });
+  }
 }
