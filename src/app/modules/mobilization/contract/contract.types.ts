3fe3cf00422eb5aae693c90511034e9a3f4b24a9
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-04-09 19:47:25 -0300

    feat: Detail contract component
    - implements new contract types

--- src/app/modules/mobilization/contract/contract.types.ts
+++ src/app/modules/mobilization/contract/contract.types.ts
@@ -1,9 +1,28 @@
 export interface ContractResource {
   id: number;
   code: string;
-  contractor: ContractorResource;
+  cnpj: string;
+  dateInitialMet: Date;
+  fantasyName: string;
   vigence: VigenceResource;
+  scope: string;
+  degreeRiskLevel: string;
+  contractManager: ContractManagerResource;
+  matrixOfResponsability: MatrixOfResponsabilityResource;
+  contractor: ContractorResource;
   // todo: add others fields for contract resource
+}
+
+export interface ContractManagerResource {
+  name: string;
+  email: string;
+  phoneNumber: string;
+}
+
+export interface MatrixOfResponsabilityResource {
+  name: string;
+  function: string;
+  email: string;
 }
 
 export interface ContractorResource {
