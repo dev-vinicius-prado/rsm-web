0ecfab17c8f5c8e4c75acd4052a34540f38ff6a4
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-04-16 13:54:32 -0300

    feat: Detail contract component
    - adjusting form contractData, step ContractManager;
    - implements new matrix of responsability.

--- src/app/modules/mobilization/contract/detail/detail-contract.component.ts
+++ src/app/modules/mobilization/contract/detail/detail-contract.component.ts
@@ -1,5 +1,5 @@
 import { CdkAccordionModule } from "@angular/cdk/accordion";
-import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault } from "@angular/common";
+import { NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, JsonPipe } from "@angular/common";
 import { Component, OnInit, ViewEncapsulation } from "@angular/core";
 import { FormArray, FormGroup, FormsModule, ReactiveFormsModule, UntypedFormBuilder, UntypedFormGroup, Validators } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
@@ -15,79 +15,19 @@ import { MatSelectModule } from "@angular/material/select";
 import { MatStepperModule } from "@angular/material/stepper";
 import { MatTableModule } from "@angular/material/table";
 import { TranslocoModule } from "@ngneat/transloco";
-import { ContractResource, MatrixOfResponsabilityResource } from "../contract.types";
-
-const DATA = {
-  id: 0,
-  code: "",
-  cnpj: "",
-  dateInitialMet: null,
-  fantasyName: "Pico Empreendimentos",
-  vigence: {
-    startAt: null,
-    finishAt: null,
-  },
-  scope: "",
-  degreeRiskLevel: null,
-  contractManager: {
-    name: "",
-    email: "",
-    phoneNumber: "",
-  },
-  matrixOfResponsability: [
-    {
-      name: "Nimasi",
-      function: "Administrador",
-      email: "email@email.com",
-    },
-    {
-      name: "Togata",
-      function: "Coordenador",
-      email: "email@email.com",
-    },
-  ],
-  contractor: {
-    name: "",
-  },
-};
-
-const COLUMNS_SCHEMA = [
-  {
-    key: "name",
-    type: "text",
-    label: "Nome",
-  },
-  {
-    key: "function",
-    type: "text",
-    label: "Função",
-  },
-  {
-    key: "email",
-    type: "text",
-    label: "E-mail",
-  },
-  {
-    key: "isEdit",
-    type: "isEdit",
-    label: "",
-  },
-];
+import { ContractResource } from "../contract.types";
 
 @Component({
   selector: "app-detail-contract",
   standalone: true,
   encapsulation: ViewEncapsulation.None,
-  imports: [TranslocoModule, MatIconModule, FormsModule, ReactiveFormsModule, MatStepperModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule, MatCheckboxModule, MatRadioModule, MatDatepickerModule, MatExpansionModule, CdkAccordionModule, MatTableModule, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault],
+  imports: [TranslocoModule, MatIconModule, FormsModule, ReactiveFormsModule, MatStepperModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatOptionModule, MatButtonModule, MatCheckboxModule, MatRadioModule, MatDatepickerModule, MatExpansionModule, CdkAccordionModule, MatTableModule, NgFor, NgIf, NgSwitch, NgSwitchCase, NgSwitchDefault, JsonPipe],
   templateUrl: "./detail-contract.component.html",
   styleUrl: "./detail-contract.component.scss",
 })
 export class DetailContractComponent implements OnInit {
-  isEditing: boolean = false;
   includeForm: UntypedFormGroup;
-  contractResource: ContractResource = DATA;
-  dataSource: any[] = this.contractResource.matrixOfResponsability;
-  displayedColsResponsibles = COLUMNS_SCHEMA.map((col) => col.key);
+  contractResource: ContractResource;
   /**
    * Constructor
    */
@@ -119,18 +59,12 @@ export class DetailContractComponent implements OnInit {
         matrixOfResponsability: this._formBuilder.array([]),
       }),
     });
+
+    this.addResponsible();
   }
 
   matrix(): FormArray {
     return this.includeForm.get("contractData").get("matrixOfResponsability") as FormArray;
-  }
-
-  newResponsible(): FormGroup {
-    return this._formBuilder.group({
-      name: [{value: "", disabled: true},],
-      function: [""],
-      email: [""],
-    });
   }
 
   addResponsible() {
@@ -141,11 +75,15 @@ export class DetailContractComponent implements OnInit {
     this.matrix().removeAt(rowIndex);
   }
 
-  isEdit() {
-    this.isEditing = !this.isEditing;
+  newResponsible(): FormGroup {
+    return this._formBuilder.group({
+      name: [""],
+      function: [""],
+      email: [""],
+    });
   }
 
   save() {
-    console.log(this.includeForm);
+    console.log(this.includeForm.value);
   }
 }
