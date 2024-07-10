4ce140878d5eda1fd11052eba17ccecac2828aa1
Vinicius Prado <developer.vinicius.prado@gmail.com>
2024-07-09 21:02:22 -0300

    refactor: Step contract data component and resource data component.
    - implements responsive layout.

--- src/app/modules/mobilization/contract/step-resource-data/step-resource-data.component.ts
+++ src/app/modules/mobilization/contract/step-resource-data/step-resource-data.component.ts
@@ -3,6 +3,7 @@ import { NgFor } from "@angular/common";
 import { Component, OnInit } from "@angular/core";
 import { FormArray, FormBuilder, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
 import { MatButtonModule } from "@angular/material/button";
+import { MatCheckbox, MatCheckboxModule } from "@angular/material/checkbox";
 import { MatExpansionModule } from "@angular/material/expansion";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatIconModule } from "@angular/material/icon";
@@ -16,26 +17,39 @@ import { TranslocoModule } from "@ngneat/transloco";
 @Component({
   selector: "step-resource-data",
   standalone: true,
-  imports: [TranslocoModule, CdkAccordionModule, MatButtonModule, MatIconModule, MatExpansionModule, NgFor, MatStepperModule, MatTableModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatRadioModule, FormsModule, ReactiveFormsModule],
+  imports: [TranslocoModule, CdkAccordionModule, MatButtonModule, MatCheckboxModule,MatIconModule, MatExpansionModule, NgFor, MatStepperModule, MatTableModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatIconModule, MatRadioModule, FormsModule, ReactiveFormsModule],
   templateUrl: "./step-resource-data.component.html",
   styleUrl: "./step-resource-data.component.scss",
 })
 export class StepResourceDataComponent implements OnInit {
   contractForm: FormGroup;
   resourceDataForm: FormGroup;
+  requiredDocuments: string[] = [
+    'Cópia do RG',
+    'Cópia do CPF',
+    'Comprovante de Endereço',
+    'Certidão de Nascimento ou Casamento',
+    'Carteira de Trabalho (CTPS)',
+    'Atestado de Saúde Ocupacional (ASO)'
+  ];
 
-  constructor(private _formBuilder: FormBuilder, private _ctrlContainer: FormGroupDirective) {}
+  selectedDocuments: { [key: string]: boolean } = {};
+
+  constructor(private _formBuilder: FormBuilder, private _ctrlContainer: FormGroupDirective) {
+    this.requiredDocuments.forEach(document => {
+      this.selectedDocuments[document] = false;
+    });
+  }
 
   ngOnInit(): void {
     this.resourceDataForm = this._formBuilder.group({
-      contractorManager: this._formBuilder.group({
-        name: ["", null],
-        email: ["", null],
-        phoneNumber: ["", null],
-      }),
-      type: ["", null],
-      function: ["", null],
-      quantity: ["", null],
+      name: ["", Validators.required],
+      cpf: ["", Validators.required],
+      identity: ["", Validators.required],
+      jobTitle: ["", Validators.required],
+      birthday: ["", Validators.required],
+      phoneNumber: ["", Validators.required],
+      email: ["", Validators.required],
       files: this._formBuilder.array([]),
     });
 
