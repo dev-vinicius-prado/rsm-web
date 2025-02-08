c41d75902dba55cf2099ad7b0bcb15b734b0c898
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-02-08 16:10:30 -0300

    feat: update user IDs in mock data to fixed values for consistency

--- src/app/mock-api/common/user/data.ts
+++ src/app/mock-api/common/user/data.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 /* eslint-disable */
 export const user = {
-    id    : uuidv4(),
+    id    : "a14aac2a-a24a-4612-bd6d-95a09980ea1b",
     name  : 'Brian Hughes',
     email : 'hughes.brian@company.com',
     avatar: 'assets/images/avatars/brian-hughes.jpg',
@@ -12,7 +12,7 @@ export const user = {
 };
 /* eslint-disable */
 export const userContract = {
-    id    : uuidv4(),
+    id    : "f5a3db06-5fed-4cef-bb5a-6879b198bccb",
     name  : 'Antonio Moreno',
     email : 'antonio.moreno@company.com',
     avatar: 'assets/images/avatars/male-02.jpg',
@@ -22,7 +22,7 @@ export const userContract = {
 };
 /* eslint-disable */
 export const userContractor = {
-    id    : uuidv4(),
+    id    : "c243e6a8-746b-4cfc-97b7-d726b869f9d1",
     name  : 'Joana Silva',
     email : 'joana.silva@contracted-company.com',
     avatar: 'assets/images/avatars/female-02.jpg',
