f8ea1b01b033fe6f732504439b3e02e3a533b224
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-02-09 11:03:35 -0300

    feat: add Contract interface to define contract data structure

---
+++ src/app/core/models/contract/contract.types.ts
@@ -0,0 +1,26 @@
+export interface Contract {
+    id: number;
+    code: string;
+    cnpj: string;
+    dateInitialMet: Date;
+    fantasyName: string;
+    vigence: {
+      startAt: Date;
+      finishAt: Date;
+    };
+    scope: string;
+    degreeRiskLevel: string;
+    contractManager: {
+      name: string;
+      email: string;
+      phoneNumber: string;
+    };
+    matrixOfResponsability: {
+      name: string;
+      function: string;
+      email: string;
+    }[];
+    contractor: {
+      name: string;
+    };
+  }
