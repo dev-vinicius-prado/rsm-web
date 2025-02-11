88af0ce49451abe209c82c459e518781ac744930
Vinicius Prado <developer.vinicius.prado@gmail.com>
2025-02-11 17:09:04 -0300

    feat: enhance contract model with detailed participant, contractor, and resource structures

--- src/app/core/models/contract/contract.types.ts
+++ src/app/core/models/contract/contract.types.ts
@@ -1,29 +1,99 @@
+interface Contact {
+    phoneNumber: string;
+    email: string;
+}
+
+interface Participant {
+    name: string;
+    function: string;
+    contact: Contact;
+}
+
+interface Vigence {
+    startAt: Date;
+    finishAt: Date;
+}
+
+interface DocumentRequired {
+    id: number;
+    description: string;
+    name: string;
+    required: boolean;
+    /**
+     * Representação binária do arquivo enviado
+     */
+    file: string;
+    downloadLink: string;
+}
+/**
+ * Empresa Contratante
+ */
+interface Contractor {
+    cnpj: string;
+    company: string;
+}
+
+interface ContractManager {
+    participant: Participant;
+}
+
+interface MatrixOfResponsability {
+    participant: Participant;
+}
+/**
+ * Empresa Contratata
+ */
+interface contracted {
+    id: number;
+    cnpj: string;
+    company: string;
+    responsibleParticipant: Participant;
+    documents: { document: DocumentRequired }[];
+}
+/**
+ * Item do recurso mobilizado
+ */
+interface Item {
+    id: number;
+    function: string;
+    quantity: number;
+    documents: { document: DocumentRequired }[];
+    racs: { rac: any }[];
+    nrs: { nr: any }[];
+}
+/**
+ * Recurso a ser mobilizado
+ */
+interface Resource {
+    id: number;
+    type: string;
+    description: string;
+    category: string; // Para Vehicle: ['PASSENGER', 'PICKUP', 'TRUCK']; Para Equipment: ['FIXED', 'MOBILE']
+    makeEmployBadge: boolean; // Emitir crachá
+    provideFood: boolean;
+    items: { item: Item }[];
+}
+
+enum ResourceTypes {
+    PERSONAL = 'PERSONAL',
+    VEHICLE = 'VEHICLE',
+    EQUIPMENT = 'EQUIPMENT',
+}
+
+interface ResourcesOfContract {
+    resources: { resource: Resource }[];
+}
+
 export interface Contract {
     id: number;
     code: string;
-    cnpj: string;
     dateInitialMet: Date;
-    fantasyName: string;
-    vigence: {
-        startAt: Date;
-        finishAt: Date;
-    };
     scope: string;
     degreeRiskLevel: string;
-    contractManager: {
-        name: string;
-        email: string;
-        phoneNumber: string;
-        company: string;
-    };
-    matrixOfResponsability: {
-        name: string;
-        function: string;
-        email: string;
-    }[];
-    contractor: {
-        name: string;
-        company: string;
-        companyId: number;
-    };
+    vigence: Vigence;
+    contractor: Contractor;
+    contractManager: ContractManager;
+    matrixOfResponsability: MatrixOfResponsability[];
+    contracted: contracted;
+    resourcesOfContract: ResourcesOfContract;
 }
